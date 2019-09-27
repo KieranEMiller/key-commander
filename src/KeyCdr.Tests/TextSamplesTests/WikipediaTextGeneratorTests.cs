5334e476c6626b10841927e678d3c344a8a2efc9
Kieran E. Miller <kieranemiller@gmail.com>
2019-09-27 20:11:00 +0100

    add basic wikipedia article lookup functionality using AngleSharp

---
+++ src/KeyCdr.Tests/TextSamplesTests/WikipediaTextGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using KeyCdr.TextSamples;
+
+namespace KeyCdr.Tests.TextSamplesTests
+{
+    [TestFixture]
+    public class WikipediaTextGeneratorTests
+    {
+        [Test]
+        public async Task test()
+        {
+            var gen = new WikipediaTextGenerator();
+            var textSections = await gen.GetIt();
+            string qwer = "qwer";
+        }
+    }
+}
---
+++ src/KeyCdr/TextSamples/WikipediaTextGenerator.cs
@@ -0,0 +1,31 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyCdr.TextSamples
+{
+    public class WikipediaTextGenerator : SimpleTextGenerator, ITextSampleGenerator
+    {
+        public const string URL = "https://en.wikipedia.org/wiki/Special:Random";
+
+        public WikipediaTextGenerator()
+        {
+            
+        }
+
+        public async Task<IEnumerable<string>> GetIt()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(URL);
+            var cellSelector = "div#mw-content-text p";
+            var cells = document.QuerySelectorAll(cellSelector);
+            
+            var titles = cells.Select(m => m.TextContent);
+            return titles;
+        }
+    }
+}
