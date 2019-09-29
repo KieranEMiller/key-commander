1187c654cd484cfe4f6ead4bef51960e1b6c997f
Kieran E. Miller <kieranemiller@gmail.com>
2019-09-29 17:21:10 +0100

    experimenting with sample html files loaded from embedded resources

--- src/KeyCdr.Tests/TextSamplesTests/WikipediaTextGeneratorTests.cs
+++ src/KeyCdr.Tests/TextSamplesTests/WikipediaTextGeneratorTests.cs
@@ -11,7 +11,7 @@ namespace KeyCdr.Tests.TextSamplesTests
     [TestFixture]
     public class WikipediaTextGeneratorTests
     {
-        [Test]
+        //[Test]
         public async Task test()
         {
             var gen = new WikipediaTextGenerator();
@@ -20,14 +20,14 @@ namespace KeyCdr.Tests.TextSamplesTests
             for (int i = 0; i < 10; i++)
             {
                 result = await gen.GetWikipediaTextFromUrl();
-                //Console.WriteLine(result.Title);
-               // Console.WriteLine(result.Url);
-                //foreach (var section in result.TextSections)
-                    //Console.WriteLine(section);
+                Console.WriteLine(result.Title);
+                Console.WriteLine(result.Url);
+                foreach (var section in result.TextSections)
+                   Console.WriteLine(string.Format("**** {0} ******", section));
             }
         }
 
-        [Test]
+        //[Test]
         public async Task wikipedia_load_sample_resource()
         {
             string resourceId = "KeyCdr.Tests.TextSamplesTests.WikipediaParsingSamples.wikipedia_sample_01_title_and_content_sections.html";
@@ -38,8 +38,19 @@ namespace KeyCdr.Tests.TextSamplesTests
             Console.WriteLine(result.Url);
             foreach (var section in result.TextSections)
                 Console.WriteLine(section);
+        }
 
-
+        [Test]
+        public async Task wikipedia_load_sample_resource2()
+        {
+            string resourceId = "KeyCdr.Tests.TextSamplesTests.WikipediaParsingSamples.wikipedia_sample_02_non_english_chars.html";
+            string html = EmbeddedResources.LoadResourceAsString(resourceId);
+            var gen = new WikipediaTextGenerator();
+            WikipediaTextResult result = await gen.GetWikipediaTextFromString(html);
+            Console.WriteLine(result.Title);
+            Console.WriteLine(result.Url);
+            foreach (var section in result.TextSections)
+                Console.WriteLine(section);
         }
     }
 }
