6bc62dc6b9092e6a5667369434556365befd6f0c
Kieran E. Miller <kieranemiller@gmail.com>
2019-09-29 18:38:20 +0100

    1) added text prefetcher that looks up additional text sections while you are typing so that there is no wait time after the app loads 2) refactored the app to run in a separate class

---
+++ src/KeyCdr.UI.ConsoleApp/KeyCdrApp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KeyCdr.Analytics;
+using KeyCdr.TextSamples;
+
+namespace KeyCdr.UI.ConsoleApp
+{
+    public class KeyCdrApp
+    {
+        public KeyCdrApp()
+        {
+            _textSampleFetcher = new TextPrefetcher();
+        }
+
+        private TextPrefetcher _textSampleFetcher;
+
+        public void RunGameLoop()
+        {
+            string selection;
+            while ((selection = ShowMenuAndPromptForNextAction()).Equals("x", StringComparison.CurrentCultureIgnoreCase) == false)
+            {
+                string textToShow = MenuSelectionToTextSample(selection);
+                Console.WriteLine("press enter when ready to begin.  good luck!");
+                Console.ReadLine();
+                Console.WriteLine(textToShow);
+
+                MeasuredKeySequence analysis = new MeasuredKeySequence(
+                    textToShow, new List<AnalyticType> { AnalyticType.Speed, AnalyticType.Accuracy }
+                );
+                analysis.Start();
+
+                string userInput = Console.ReadLine();
+
+                var results = analysis.Stop(userInput);
+                ShowResults(analysis, results);
+            }
+        }
+
+        private string MenuSelectionToTextSample(string selection)
+        {
+            string textToShow = string.Empty;
+            if (selection.Equals("1")) textToShow = _textSampleFetcher.GetWord();
+            else if (selection.Equals("2")) textToShow = _textSampleFetcher.GetSentence();
+            else if (selection.Equals("3")) textToShow = _textSampleFetcher.GetParagraph();
+
+            return textToShow;
+        }
+
+        private string ShowMenuAndPromptForNextAction()
+        {
+            Console.WriteLine("Welcome to Key Commander");
+            Console.WriteLine();
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Practice Words");
+            Console.WriteLine("2. Practice Sentences");
+            Console.WriteLine("3. Practice Paragraphs");
+            Console.WriteLine("X to quit");
+            return Console.ReadLine();
+        }
+
+        private void ShowResults(MeasuredKeySequence whatToMeasure, IList<KeyCdr.Analytics.IAnalytic> results)
+        {
+            Console.WriteLine();
+            Console.WriteLine("your results:");
+            foreach (var result in results)
+                Console.WriteLine("\t{0}", result.GetResultSummary());
+
+            Console.WriteLine();
+            Console.WriteLine("press enter when ready to continue");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ src/KeyCdr.UI.ConsoleApp/TextPrefetcher.cs
@@ -0,0 +1,93 @@
+﻿using KeyCdr.TextSamples;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace KeyCdr.UI.ConsoleApp
+{
+    public class TextPrefetcher
+    {
+        public int NUM_PREFETCHES_TO_KEEP_ON_HAND = 3;
+
+        public TextPrefetcher()
+        {
+            _wikiTextGenerator = new WikipediaTextGenerator();
+            _cachedData = new Queue<WikipediaTextResult>();
+
+            Init();
+            PrefetchMoreIfNeeded();
+        }
+
+        private WikipediaTextGenerator _wikiTextGenerator;
+        private WikipediaTextResult _wikiText;
+
+        private Queue<WikipediaTextResult> _cachedData;
+
+        private void Init()
+        {
+            _wikiText = _wikiTextGenerator.GetWikipediaTextFromUrlSynchronously();
+        }
+
+        private async void PrefetchMoreIfNeeded()
+        {
+            for(int i=1; i <= NUM_PREFETCHES_TO_KEEP_ON_HAND - _cachedData.Count; i++)
+            {
+                var data = await _wikiTextGenerator.GetWikipediaTextFromUrl();
+                
+                _cachedData.Enqueue(data);
+                if (_wikiText == null)
+                    _wikiText = data;
+            }
+        }
+
+        private string GetNextTextSection()
+        {
+            string textSection = string.Empty;
+            int counter = 0;
+            while(string.IsNullOrEmpty(textSection) && counter < 5)
+            {
+                if(_wikiText == null || _wikiText.TextSections.Count == 0)
+                {
+                    _wikiText = _cachedData.Dequeue();
+                }
+                else
+                {
+                    textSection = _wikiText.TextSections[0];
+                    _wikiText.TextSections.RemoveAt(0);
+                    break;
+                }
+                
+                counter++;
+            }
+
+            PrefetchMoreIfNeeded();
+
+            return textSection;
+        }
+
+        public string GetWord()
+        {
+            string text = GetNextTextSection();
+            var simple = new SimpleTextGenerator(text);
+            string word = simple.GetWord();
+            return word;
+        }
+        
+        public string GetSentence()
+        {
+            string text = GetNextTextSection();
+            var simple = new SimpleTextGenerator(text);
+            string sentence = simple.GetSentence();
+            return sentence;
+        }
+
+        public string GetParagraph()
+        {
+            string text = GetNextTextSection();
+            var simple = new SimpleTextGenerator(text);
+            string paragraph = simple.GetParagraph();
+            return paragraph;
+        }
+    }
+}
