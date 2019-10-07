c2c22725471ceb8dbf7a6d1ed84c8eb6182dec96
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-07 21:07:24 +0100

    1) add login modal when the app starts up to prompt the user for their id or to use a guest user 2) add supporting models, viewmodels for the login and main windows

---
+++ src/KeyCdr.UI.WPF/Models/MainModel.cs
@@ -0,0 +1,18 @@
+﻿using KeyCdr.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyCdr.UI.WPF.Models
+{
+    public class MainModel
+    {
+        public MainModel()
+        { }
+
+        public KCUser User { get; set; }
+        public IList<Session> RecentSessions { get; set; }
+    }
+}
