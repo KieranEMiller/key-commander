9ee25774e298a3811ed4794185f688a14cf55739
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-07 21:10:53 +0100

    1) add UserSessionHistory to lookup recent sessions by user 2) add UserSession to wrap the logic for tracking multiple key sequences by a user in a period of time

---
+++ src/KeyCdr/UserSession.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyCdr.Data;
+using KeyCdr.TextSamples;
+using KeyCdr.Analytics;
+
+namespace KeyCdr
+{
+    public class UserSession : BaseDBAccess
+    {
+        public UserSession()
+            : this(null)
+        { }
+
+        public UserSession(KCUser user)
+            : this(user, new WikipediaTextGenerator())
+        { } 
+
+        public UserSession(KCUser user, ITextSampleGenerator generator)
+        {
+            _user = user;
+            _generator = generator;
+        }
+
+        private ITextSampleGenerator _generator;
+
+        private MeasuredKeySequence _currentSeq;
+
+        private Data.KCUser _user;
+        private Data.Session _session;
+        private Data.KeySequence _dbSeq;
+
+        public void NewSession()
+        {
+            _session = new Session();
+            _session.SessionId = Guid.NewGuid();
+            _session.UserId = _user.UserId;
+            _session.Created = DateTime.Now;
+            _db.Sessions.Add(_session);
+        }
+
+        public void StartNewSequence(ITextSample sample)
+        {
+            if (_session == null) NewSession();
+
+            _dbSeq = new KeySequence();
+            _dbSeq.KeySequenceId = Guid.NewGuid();
+            _dbSeq.Session = _session;
+            _dbSeq.SourceKey = sample.GetSourceKey();
+            _dbSeq.TextShown = sample.GetText();
+            _dbSeq.SourceTypeId = (int) sample.GetSourceType();
+            _dbSeq.Created = System.DateTime.Now;
+            _dbSeq.TextEntered = null;
+
+            _db.KeySequences.Add(_dbSeq);
+            _db.SaveChanges();
+
+            _currentSeq = new MeasuredKeySequence(sample.GetText());
+            _currentSeq.Start();
+        }
+
+        public IList<IAnalytic> Stop(string entered)
+        {
+            IList<IAnalytic> results = _currentSeq.Stop(entered);
+            _dbSeq.TextEntered = entered;
+
+            foreach(var analytic in results)
+                RecordAnalytic(analytic);
+
+            _db.SaveChanges();
+
+            return results;
+        }
+
+        public void RecordAnalytic(IAnalytic analytic)
+        {
+            Data.KeySequenceAnalysis analysis = new Data.KeySequenceAnalysis();
+            analysis.KeySequenceAnalysisId = Guid.NewGuid();
+            analysis.KeySequenceId = _dbSeq.KeySequenceId;
+            analysis.Created = DateTime.Now;
+            analysis.AnalysisTypeId = (int)analytic.GetAnalyticType();
+            _db.KeySequenceAnalysises.Add(analysis);
+
+            if (analytic is IAnalyticRecordable)
+                ((IAnalyticRecordable)analytic).Record(_db, analysis);
+        }
+
+        public ITextSample GetWord()
+        { return _generator.GetWord(); }
+
+        public ITextSample GetSentence()
+        { return _generator.GetSentence(); }
+
+        public ITextSample GetParagraph()
+        { return _generator.GetParagraph(); }
+    }
+}
---
+++ src/KeyCdr/Users/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyCdr.Data;
+
+namespace KeyCdr.Users
+{
+    public class UserManager: BaseDBAccess
+    {
+        public UserManager()
+        {
+        }
+
+        public KCUser GetByLoginName(string loginName)
+        {
+            KCUser user = _db.KCUsers
+                .Where(u => u.LoginName.Equals(loginName))
+                .FirstOrDefault();
+
+            return user;
+        }
+
+        public KCUser CreateGuest()
+        {
+            KCUser user = new KCUser();
+            user.UserId = Guid.NewGuid();
+
+            string guestname = GetNewGuestName();
+            user.Name = guestname;
+            user.LoginName = guestname;
+            user.Created = DateTime.Now;
+
+            _db.KCUsers.Add(user);
+            _db.SaveChanges();
+
+            return user;
+        }
+
+        public string GetNewGuestName()
+        {
+            return string.Format("guest_{0}", DateTime.UtcNow.Ticks);
+        }
+    }
+}
