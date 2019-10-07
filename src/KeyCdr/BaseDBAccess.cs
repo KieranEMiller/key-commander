2e7185e05dd17f2167cd46706fad7de7a481a9eb
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-07 20:54:51 +0100

    add a base class for classes that need db access; handles init and disposal of the db context

---
+++ src/KeyCdr/BaseDBAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyCdr.Data;
+
+namespace KeyCdr
+{
+    public class BaseDBAccess
+    {
+        public BaseDBAccess()
+        {
+            _db = new KeyCommanderEntities();
+            #if DEBUG
+                _db.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+            #endif
+        }
+
+        ~BaseDBAccess()
+        {
+            if (_db != null)
+                _db.Dispose();
+        }
+
+        protected KeyCommanderEntities _db;
+    }
+}
