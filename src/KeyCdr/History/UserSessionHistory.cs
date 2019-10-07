deedaf423a535daffe908104ec7cbd5581a55aa3
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-07 21:10:53 +0100

    1) add UserSessionHistory to lookup recent sessions by user 2) add UserSession to wrap the logic for tracking multiple key sequences by a user in a period of time

---
+++ src/KeyCdr/History/UserSessionHistory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyCdr.Data;
+
+namespace KeyCdr.History
+{
+    public class UserSessionHistory : BaseDBAccess
+    {
+        public UserSessionHistory()
+        { }
+
+        public IList<Data.Session> GetSessionsByUser(Data.KCUser currentUser)
+        {
+            var results = _db.Sessions
+                .Where(s => s.UserId.Equals(currentUser.UserId))
+                .Include(s => s.KeySequences)
+                .OrderByDescending(s => s.Created);
+
+            return results.ToList();
+
+                    /*
+                    (session, user) => new
+                    {
+                        session.SessionId,
+                        user.UserId,
+                        session.Created
+                    }*/
+        }
+    }
+}
