df1bc1597a03f7a5891850c9a6abcaa67b83405e
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-14 22:02:12 +0100

    break up logic into distinct method to ease readibility

--- src/KeyCdr/Highlighting/HighlightDeterminator.cs
+++ src/KeyCdr/Highlighting/HighlightDeterminator.cs
@@ -21,36 +21,58 @@ namespace KeyCdr.Highlighting
 
             foreach(var measurement in accuracy.Measurements)
             {
-                foreach(var incorrectChar in measurement.IncorrectChars)
-                {
-                    int indexStart = incorrectChar.Index + measurement.IndexInLargerText;
-                    details.Add(new HighlightDetail() {
-                        HighlightType = HighlightType.IncorrectChar,
-                        IndexStart = indexStart,
-                        IndexEnd = indexStart + 1
-                    });
-                }
+                details.AddRange(ProcessIncorrectChars(measurement));
 
+                //the highlighted text for extra and short characters is similar; either
+                //  1) short: length measured + number short or missing
+                //  2) extra: length measured + number extra chars
                 if(measurement.NumExtraChars > 0 || measurement.NumShortChars > 0)
                 {
-                    //the highlighted text for extra and short characters is similar; either
-                    //  1) short: length measured + number short or missing
-                    //  2) extra: length measured + number extra chars
-                    int indexStart = measurement.LengthMeasured - 1;
-                    int indexEnd = indexStart + ((measurement.NumExtraChars > 0) ? 
-                        measurement.NumExtraChars 
-                        : measurement.NumShortChars);
-
-                    details.Add(new HighlightDetail() {
-                        HighlightType = HighlightType.ExtraChars,
-                        IndexStart = indexStart,
-                        IndexEnd = indexEnd
-                    });
+                    details.AddRange(ProcessExtraAndShortChars(measurement));
                 }
             }
 
             return details;
         }
 
+        public IList<HighlightDetail> ProcessIncorrectChars(AccuracyMeasurement measurement)
+        {
+            var details = new List<HighlightDetail>();
+            foreach(var incorrectChar in measurement.IncorrectChars)
+            {
+                int indexStart = incorrectChar.Index + measurement.IndexInLargerText;
+                details.Add(new HighlightDetail() {
+                    HighlightType = HighlightType.IncorrectChar,
+                    IndexStart = indexStart,
+                    IndexEnd = indexStart + 1
+                });
+            }
+            return details;
+        }
+
+        public IList<HighlightDetail> ProcessExtraAndShortChars(AccuracyMeasurement measurement)
+        {
+            var details = new List<HighlightDetail>();
+
+            //are we working with extra or short chars
+            bool isExtraChars = (measurement.NumExtraChars > 0);
+
+            int delta = (isExtraChars) ? 
+                measurement.NumExtraChars 
+                : measurement.NumShortChars;
+
+            int indexStart = measurement.LengthMeasured - delta;
+            int indexEnd = indexStart + delta;
+
+            details.Add(new HighlightDetail() {
+                HighlightType = (isExtraChars) ? 
+                    HighlightType.ExtraChars 
+                    : HighlightType.ShortChars,
+                IndexStart = indexStart,
+                IndexEnd = indexEnd
+            });
+
+            return details;
+        }
     }
 }
