66cc925e4305193bbd460302c0190f8fd9cf5409
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-14 21:58:12 +0100

    add class to convert a HighlightType to a brush color

---
+++ src/KeyCdr.UI.WPF/Util/HighlightBrushFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace KeyCdr.UI.WPF.Util
+{
+    public class HighlightBrushFactory
+    {
+        public Brush HighlightToBrush(Highlighting.HighlightType type)
+        {
+            if (type == Highlighting.HighlightType.ExtraChars)
+                return Brushes.Azure;
+
+            else if (type == Highlighting.HighlightType.IncorrectChar)
+                return Brushes.Red;
+
+            else if (type == Highlighting.HighlightType.ShortChars)
+                return Brushes.Orange;
+
+            else
+                throw new ArgumentException(string.Format("unknown highlight type {0}", type.ToString()));
+        }
+    }
+}
