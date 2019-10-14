489fef152cea36fc0bd6f0c6c3aa646161065bc2
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-14 21:20:40 +0100

    add subclass of TextBlock that allows you to bind the Inline property; inline allows you to format pieces of text displayed in the textblock; will be used to show the user what they got wrong after running through a sequence of text

---
+++ src/KeyCdr.UI.WPF/Util/BindableTextBlock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace KeyCdr.UI.WPF.Util
+{
+    //not my own work, taken from 
+    //https://stackoverflow.com/questions/1959856/data-binding-the-textblock-inlines
+    public class BindableTextBlock : TextBlock
+    {
+        public ObservableCollection<Inline> InlineList
+        {
+            get { return (ObservableCollection<Inline>)GetValue(InlineListProperty); }
+            set { SetValue(InlineListProperty, value); }
+        }
+
+        public static readonly DependencyProperty InlineListProperty =
+            DependencyProperty.Register("InlineList", typeof(ObservableCollection<Inline>), typeof(BindableTextBlock), new UIPropertyMetadata(null, OnPropertyChanged));
+
+        private static void OnPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            BindableTextBlock textBlock = (BindableTextBlock)sender;
+            textBlock.Inlines.Clear();
+            textBlock.Inlines.AddRange((ObservableCollection<Inline>)e.NewValue);
+        }
+    }
+}
