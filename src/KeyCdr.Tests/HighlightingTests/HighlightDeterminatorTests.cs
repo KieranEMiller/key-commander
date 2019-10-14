77c67b3de02ffd1b839a71a25102112523d2da21
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-14 22:00:58 +0100

    add tests for new highlight classes

--- src/KeyCdr.Tests/HighlightingTests/HighlightDeterminatorTests.cs
+++ src/KeyCdr.Tests/HighlightingTests/HighlightDeterminatorTests.cs
@@ -23,15 +23,65 @@ namespace KeyCdr.Tests.HighlightingTests
             Assert.That(det.Compute(accuracy), Is.Empty);
         }
 
-        [Test]
-        public void one_word_one_incorrect_char()
+        [TestCase("abc", "abz", 2, 3)]
+        [TestCase("abc", "zbc", 0, 1)]
+        [TestCase("abc", "azc", 1, 2)]
+        public void one_word_one_incorrect_char_at_all_positions(
+            string shown, string entered, int start, int end)
         {
-            var accuracy = new Accuracy(new AnalyticData() { TextShown = "abc", TextEntered = "abq" });
+            var accuracy = new Accuracy(new AnalyticData() { TextShown = shown, TextEntered = entered});
             accuracy.Compute();
 
             HighlightDeterminator det = new HighlightDeterminator();
             var details = det.Compute(accuracy);
-            
+            Assert.That(details.Count, Is.EqualTo(1));
+            Assert.That(details, Is.EquivalentTo(new List<HighlightDetail>() {
+                new HighlightDetail() {
+                    HighlightType=HighlightType.IncorrectChar,
+                    IndexStart=start,
+                    IndexEnd=end,
+                }
+            }));
+        }
+
+        [TestCase("abc", "ab", 2, 3)]
+        [TestCase("abc", "a", 1, 3)]
+        public void one_word_one_short_chars(
+            string shown, string entered, int start, int end)
+        {
+            var accuracy = new Accuracy(new AnalyticData() { TextShown = shown, TextEntered = entered});
+            accuracy.Compute();
+
+            HighlightDeterminator det = new HighlightDeterminator();
+            var details = det.Compute(accuracy);
+            Assert.That(details.Count, Is.EqualTo(1));
+            Assert.That(details, Is.EquivalentTo(new List<HighlightDetail>() {
+                new HighlightDetail() {
+                    HighlightType=HighlightType.ShortChars,
+                    IndexStart=start,
+                    IndexEnd=end,
+                }
+            }));
+        }
+
+        [TestCase("abc", "abcd", 3, 4)]
+        [TestCase("abc", "abcdefg", 3, 7)]
+        public void one_word_one_extra_chars(
+            string shown, string entered, int start, int end)
+        {
+            var accuracy = new Accuracy(new AnalyticData() { TextShown = shown, TextEntered = entered });
+            accuracy.Compute();
+
+            HighlightDeterminator det = new HighlightDeterminator();
+            var details = det.Compute(accuracy);
+            Assert.That(details.Count, Is.EqualTo(1));
+            Assert.That(details, Is.EquivalentTo(new List<HighlightDetail>() {
+                new HighlightDetail() {
+                    HighlightType=HighlightType.ExtraChars,
+                    IndexStart=start,
+                    IndexEnd=end,
+                }
+            }));
         }
     }
 }
---
+++ src/KeyCdr.Tests/HighlightingTests/HighlightedTextBuilderTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyCdr.Analytics;
+using KeyCdr.Highlighting;
+using NUnit.Framework;
+
+namespace KeyCdr.Tests.HighlightingTests
+{
+    [TestFixture]
+    public class HighlightedTextBuilderTests
+    {
+        public IList<HighlightDetail> run_determinator(string shown, string entered)
+        {
+            var accuracy = new Accuracy(new AnalyticData() { TextShown = shown, TextEntered = entered });
+            accuracy.Compute();
+
+            HighlightDeterminator det = new HighlightDeterminator();
+            IList<HighlightDetail> details = det.Compute(accuracy);
+            return details;
+        }
+
+        [Test]
+        public void single_word_simple_incorrect_char_start_of_word()
+        {
+            var details = run_determinator("abc", "xbc");
+
+            HighlightedTextBuilder builder = new HighlightedTextBuilder();
+            Queue<HighlightedText> result = builder.Compute("abc", details);
+
+            Assert.That(result, Is.EquivalentTo(
+                new Queue<HighlightedText>(
+                    new List<HighlightedText>(){
+                        new HighlightedText() {
+                            HighlightType = HighlightType.IncorrectChar,
+                            Text = "a",
+                        },
+                        new HighlightedText() {
+                            HighlightType = HighlightType.Normal,
+                            Text="bc"
+                        }
+                    }
+                )
+            ));
+        }
+
+        [Test]
+        public void single_word_simple_incorrect_char_middle_of_word()
+        {
+            var details = run_determinator("abc", "axc");
+
+            HighlightedTextBuilder builder = new HighlightedTextBuilder();
+            Queue<HighlightedText> result = builder.Compute("abc", details);
+
+            Assert.That(result, Is.EquivalentTo(
+                new Queue<HighlightedText>(
+                    new List<HighlightedText>(){
+                        new HighlightedText() {
+                            HighlightType = HighlightType.Normal,
+                            Text = "a",
+                        },
+                        new HighlightedText() {
+                            HighlightType = HighlightType.IncorrectChar,
+                            Text="b"
+                        },
+                        new HighlightedText() {
+                            HighlightType = HighlightType.Normal,
+                            Text="c"
+                        }
+                    }
+                )
+            ));
+        }
+
+
+    }
+}
