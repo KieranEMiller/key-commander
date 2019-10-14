aa4e7c8641721089a0220563d6fb36b7aba9e633
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-14 22:00:19 +0100

    add functionality for rebuilding the original text shown to the user incorporating the highlighted sections

---
+++ src/KeyCdr/Highlighting/HighlightedText.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyCdr.Highlighting
+{
+    public class HighlightedText
+    {
+        public string Text { get; set; }
+        public HighlightType HighlightType { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            return Equals((HighlightedText)obj);
+        }
+
+        public bool Equals(HighlightedText obj)
+        {
+            return (this.Text == obj.Text
+                && this.HighlightType == obj.HighlightType) ;
+        }
+
+        public override int GetHashCode()
+        {
+            return new { this.Text, this.HighlightType }.GetHashCode();
+        }
+    }
+}
---
+++ src/KeyCdr/Highlighting/HighlightedTextBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyCdr.Highlighting
+{
+    public class HighlightedTextBuilder
+    {
+        public Queue<HighlightedText> Compute(string origText, IList<HighlightDetail> details)
+        {
+            Queue<HighlightedText> textSections = new Queue<HighlightedText>();
+
+            int lastIndex = 0;
+            foreach (HighlightDetail highlight in details.OrderBy(d => d.IndexStart))
+            {
+                string firstTextSection = origText.Substring(lastIndex, highlight.IndexStart);
+                if(!string.IsNullOrWhiteSpace(firstTextSection)) {
+                    HighlightedText nextUnformattedBlock = new HighlightedText() {
+                        Text = firstTextSection,
+                        HighlightType = HighlightType.Normal
+                    };
+                    textSections.Enqueue(nextUnformattedBlock);
+                }
+
+                HighlightedText formattedBlock = new HighlightedText() {
+                    Text = origText.Substring(highlight.IndexStart, highlight.IndexEnd - highlight.IndexStart),
+                    HighlightType = highlight.HighlightType
+                };
+                textSections.Enqueue(formattedBlock);
+
+                lastIndex += highlight.IndexEnd - highlight.IndexStart;
+
+                if(!string.IsNullOrWhiteSpace(firstTextSection))
+                    lastIndex++;
+            }
+
+            HighlightedText lastUnformattedBlock = new HighlightedText() {
+                Text=origText.Substring(lastIndex, origText.Length - lastIndex),
+                HighlightType = HighlightType.Normal
+            };
+            textSections.Enqueue(lastUnformattedBlock);
+
+            return textSections;
+        }
+    }
+}
