fae87dc6f14efab750defad983b2678d8172e60d
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-21 13:16:09 +0100

    change verbosity from quiet to verbose; verbose includes too much useful information to not use it by default

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@
 ECHO running build.ps1
 ECHO %DATE% %TIME%
 
-Powershell -executionpolicy remotesigned ./build.ps1 --verbosity=quiet > build.log 2>&1 & type build.log
+Powershell -executionpolicy remotesigned ./build.ps1 --verbosity=verbose > build.log 2>&1 & type build.log
