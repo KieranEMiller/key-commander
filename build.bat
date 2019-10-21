d633438c325ba46a7519e0d79aec0cc3ea4eb066
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-21 13:01:50 +0100

    build script entry point changes 1) turn off echo 2) print the day and time of execution 3) change the verbosity default to quiet

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-@ECHO ON
+@ECHO OFF
 
 ECHO running build.ps1
-Powershell -executionpolicy remotesigned ./build.ps1 > build.log 2>&1 & type build.log
+ECHO %DATE% %TIME%
+
+Powershell -executionpolicy remotesigned ./build.ps1 --verbosity=quiet > build.log 2>&1 & type build.log
