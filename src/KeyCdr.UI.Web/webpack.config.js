54efdf96d85ffde62dfb8861b7106a2ab9991306
Kieran E. Miller <kieranemiller@gmail.com>
2019-10-28 22:09:42 +0100

    add react, webpack, and babel dependencies

---
+++ src/KeyCdr.UI.Web/webpack.config.js
@@ -0,0 +1,39 @@
+﻿"use strict";
+
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+    entry: "./assets/js/react/dev/index.jsx",
+    output: {
+        path: path.resolve(__dirname, "./assets/js/react/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    /*
+    externals: {
+        // Don't bundle the 'react' npm package with the component.
+        'react': 'React'
+    },
+    resolve: {
+        // Include empty string '' to resolve files by their explicit extension
+        // (e.g. require('./somefile.ext')).
+        // Include '.js', '.jsx' to resolve files by these implicit extensions
+        // (e.g. require('underscore')).
+        extensions: ['.js', '.jsx']
+    },
+    */
+    devtool: "inline-source-map",
+    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+};
