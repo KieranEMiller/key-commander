bea129842469ef4d94d5d3132d0deeadabbacf10
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-02 06:23:31 +0100

    add new component, SecureRoute, that wraps the Route component but validates if the user is first authenticated

---
+++ src/KeyCdr.UI.Web/assets/js/react/dev/secure_route.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Auth from './auth.jsx';
+import { Route, Redirect} from 'react-router-dom'
+
+const SecureRoute = ({ component: Component, ...props }) => {
+    var auth = new Auth();
+    return (
+        <Route
+            {...props}
+            render={innerProps =>
+                auth.IsAuthenticated() ?
+                    <Component {...innerProps} />
+                    :
+                    <Redirect to="/Login" />
+            }
+        />
+    );
+};
+
+export default SecureRoute;
