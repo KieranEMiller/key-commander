d1a991d04395c3e3a8151c7768b2229030b3f857
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-02 06:22:46 +0100

    add a new route to be the users account page

---
+++ src/KeyCdr.UI.Web/assets/js/react/dev/route_my_acct.jsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from      './header.jsx';
+import ContentContainer from './content.jsx';
+
+class MyAccount extends React.Component {
+    render() {
+        return (
+            <ContentContainer>
+                <div className="content_row">
+                    <h2>My Account</h2>
+                    this should be a private only accessible page once the user has logged in
+                </div>
+            </ContentContainer>
+        );
+    }
+}
+
+export default MyAccount;
