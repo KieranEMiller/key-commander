01fe2791409968e28aeccdf2769ba3e6ad444da3
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-02 06:27:30 +0100

    add a logout link that logs the user out by removing the jwt from local storage; redirect currently is not working properly

--- src/KeyCdr.UI.Web/assets/js/react/dev/header-nav-bar.jsx
+++ src/KeyCdr.UI.Web/assets/js/react/dev/header-nav-bar.jsx
@@ -2,16 +2,26 @@
 import ReactDOM from 'react-dom';
 import { Link } from "react-router-dom";
 
+import Auth from './auth.jsx'
+
 class HeaderNavBar extends React.Component {
-    constructor(params) {
-        super(params);
+    constructor(props) {
+        super(props);
+        console.log("nav bar props", props);
         this.state = {
             routes: [
                 { route: "/Index", display: "Home", active: false },
-                { route: "/Account", display: "My Account", active: false },
+                { route: "/secure/MyAccount", display: "My Account", active: false },
                 { route: "/About", display: "About", active: false }
             ]
         };
+    };
+    logout() {
+        console.log('logging out');
+        var auth = new Auth();
+        auth.Logout();
+
+        this.context.history.push('/');
     };
     clickHandler(route) {
         this.setState(state => {
@@ -35,6 +45,9 @@ class HeaderNavBar extends React.Component {
                                 </li>
                             )   
                         })}
+                        <li>
+                            <a href='#' onClick={this.logout}>Logout</a>
+                        </li>
                     </ul>
                 </div>
             </React.Fragment>
