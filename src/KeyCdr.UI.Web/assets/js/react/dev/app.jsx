fbded0389b9dd661becfaa5e4d0a9c8c33e6295b
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-02 06:25:30 +0100

    move header from the app component to routing and add a secure route to account

--- src/KeyCdr.UI.Web/assets/js/react/dev/app.jsx
+++ src/KeyCdr.UI.Web/assets/js/react/dev/app.jsx
@@ -11,7 +11,6 @@ class App extends React.Component {
     render() {
         return (
             <Router>
-                <Header loc={this.props.location} /> 
                 <Routing />
             </Router>
         ); 
--- src/KeyCdr.UI.Web/assets/js/react/dev/routing.jsx
+++ src/KeyCdr.UI.Web/assets/js/react/dev/routing.jsx
@@ -2,19 +2,25 @@
 import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
+import SecureRoute from './secure_route.jsx';
 import Header from      './header.jsx';
+
 import Index from       './route_index.jsx';
 import About from       './route_about.jsx';
 import Login from       './route_login.jsx';
+import MyAccount from   './route_my_acct.jsx';
 
 class Routing extends React.Component {
     render() {
+        console.log("routing props", this.props);
         return (
             <div>
+                <Header loc={this.props.location} /> 
                 <Switch>
                     <Route exact path="/" component={Index} />
                     <Route exact path="/About" component={About} />
                     <Route exact path="/Login" component={Login} />
+                    <SecureRoute exact path="/secure/MyAccount" component={MyAccount} />
                     <Route component={Index} />
                 </Switch>
             </div>
