cd277eb2f387510e314cd7c2a3788bf2988b4a61
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-05 20:28:52 +0000

    adjust how authentication is handled 1) create a new base component that makes a call to the server to verify the jwt is valid 2) change the secure account page to extend the base class

--- src/KeyCdr.UI.Web/assets/js/react/dev/route_my_acct.jsx
+++ src/KeyCdr.UI.Web/assets/js/react/dev/route_my_acct.jsx
@@ -1,19 +1,31 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
+import { Redirect} from 'react-router-dom'
 
-import Header from      './header.jsx';
 import ContentContainer from './content.jsx';
+import SecureRouteComponent from './secure_route_component.jsx';
 
-class MyAccount extends React.Component {
+class MyAccount extends SecureRouteComponent {
+    constructor(props) {
+        super(props);
+
+    }
     render() {
-        return (
-            <ContentContainer>
-                <div className="content_row">
-                    <h2>My Account</h2>
-                    this should be a private only accessible page once the user has logged in
-                </div>
-            </ContentContainer>
-        );
+        if (this.authenticationComplete()) {
+            return (
+                <ContentContainer authed={true}>
+                    <div className="content_row">
+                        <h2>My Account</h2>
+                        this should be a private only accessible page once the user has logged in
+                    </div>
+                </ContentContainer>
+            );
+        }
+        else {
+            return (
+                super.render()
+            )
+        }
     }
 }
 
---
+++ src/KeyCdr.UI.Web/assets/js/react/dev/secure_route_component.jsx
@@ -0,0 +1,57 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import { Redirect } from 'react-router-dom'
+
+import Header from './header.jsx';
+import ContentContainer from './content.jsx';
+import Auth from './auth.jsx';
+
+class SecureRouteComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.auth = new Auth();
+        this.state = {
+            isAuthenticated: false,
+            authFailed: undefined
+        }
+
+        var that = this;
+        this.auth.isAuthenticated()
+            .then(ret => {
+                that.setState(() => ({
+                    isAuthenticated: ret,
+                    authFailed: !ret
+                }));
+            });
+    }
+
+    authenticationComplete() {
+        if (this.state.isAuthenticated === true
+            && this.state.authFailed === false)
+            return true;
+
+        return false;
+    }
+
+    render() {
+        if (!this.state.isAuthenticated) {
+            if (this.state.authFailed) {
+                return (
+                    <Redirect to={{ pathname: '/login', state: { from: '/secure/MyAccount' } }} />
+                )
+            }
+            else {
+                return (
+                    <ContentContainer>
+                    <div className="content_row">
+                        <h2>Authenticating...</h2>
+                    </div>
+                    </ContentContainer>
+                )
+            }
+        }
+    }
+}
+
+export default SecureRouteComponent;
