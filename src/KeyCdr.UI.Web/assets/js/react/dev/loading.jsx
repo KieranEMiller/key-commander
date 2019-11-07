13323b36048f44b3fb302728a90fe8a115909ea1
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-07 20:42:22 +0000

    add new loading component. image is currently not being used but is available in case the pure css solution doesnt pan out on multiple devices

---
+++ src/KeyCdr.UI.Web/assets/js/react/dev/loading.jsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Loading extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showLoading: true
+        }
+    }
+
+    render() {
+        if (this.state.showLoading) {
+            return (
+                <React.Fragment>
+                    <div className="loading_mask">
+                        <div className="spinner-border"></div>
+                    </div>
+                </React.Fragment>
+            )
+        }
+    }
+}
+
+export default Loading;
