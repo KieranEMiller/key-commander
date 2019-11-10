51d180b72d31f879d80dc3963f02cf116f54523f
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-10 20:23:47 +0000

    add to login: 1) loading spinner that covers the entire form when the user clicks submit 2) add an error message when the login fails for whatever reason

--- src/KeyCdr.UI.Web/assets/js/react/dev/paths/login.jsx
+++ src/KeyCdr.UI.Web/assets/js/react/dev/paths/login.jsx
@@ -3,33 +3,49 @@ import ReactDOM from 'react-dom';
 
 import ContentContainer from '../content.jsx';
 import Auth from '../auth.jsx';
+import Loading from '../loading.jsx';
 
 class Login extends React.Component {
     constructor(params) {
         super(params);
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            showLoading: false,
+            errorMsg: ''
         };
         this.AuthService = new Auth();
     }
 
     handleSubmit = (e) => {
         var logindata = { username: this.state.username, password: this.state.password }; 
-        console.log("submitting", logindata);
 
+        this.setState({ errorMsg: '' });
+        this.showLoading(true);
+
+        var that = this;
         this.AuthService.login(logindata)
             .then(data => {
-                if (data.isvalid)
-                    this.props.history.push('/secure/MyAccount');
-                else
-                    alert("unable to login");
+                setTimeout(function () {
+                    that.showLoading(false);
+
+                    if (data.isvalid)
+                        that.props.history.push('/secure/MyAccount');
+                    else
+                        that.setState({ errorMsg: 'unable to login with the username and password provided' });
+
+                }, 1000);
             })
             .catch(err => {
                 console.log(err);
             });
+            
 
         e.preventDefault();
+    }
+
+    showLoading = (showIt) => {
+        this.setState({ showLoading: showIt });
     }
 
     handleChange = (e) => {
@@ -39,25 +55,35 @@ class Login extends React.Component {
     }
 
     render() {
+
         return (
             <ContentContainer>
                 <div className="center">
                     <h2>Login</h2>
-                    <form onSubmit={this.handleSubmit} className="med_width">
-                        <label htmlFor="username">User Name: </label>
-                        <input
-                            name="username"
-                            type="text"
-                            onChange={this.handleChange} />
+                    <div className="content_row">
+                        <Loading showLoading={this.state.showLoading}/>
+                        <form onSubmit={this.handleSubmit} className="med_width">
+                            <label htmlFor="username">User Name: </label>
+                            <input
+                                name="username"
+                                type="text"
+                                onChange={this.handleChange} />
 
-                        <label htmlFor="password">Password: </label>
-                        <input
-                            name="password"
-                            type="password"
-                            onChange={this.handleChange} />
+                            <label htmlFor="password">Password: </label>
+                            <input
+                                name="password"
+                                type="password"
+                                onChange={this.handleChange} />
 
-                        <input onClick={this.handleSubmit} className="button-size-medium" type="submit" value="Login" />
-                    </form>
+                            {(this.state.errorMsg == '')
+                                ? ''
+                                : <p className="form_error">{this.state.errorMsg}</p>
+                            }
+
+                            <input onClick={this.handleSubmit} className="button-size-medium" type="submit" value="Login" />
+                            <div className="clear_both"></div>
+                        </form>
+                    </div>
                 </div>
             </ContentContainer>
         );
