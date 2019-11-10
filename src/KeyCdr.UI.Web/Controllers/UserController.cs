68c42c5407d0c3700142db36e9f785ef53442907
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-10 21:22:48 +0000

    add new user controller to retrieve recent history and update history route to use it

---
+++ src/KeyCdr.UI.Web/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace KeyCdr.UI.Web.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage History(JWTToken token)
+        {
+            var userMgr = new KeyCdr.History.UserSessionHistory();
+            IList<Data.Session> history = userMgr.GetSessionsByUser(new Data.KCUser() { UserId = Guid.Parse(token.UserId) });
+            return Request.CreateResponse(HttpStatusCode.OK, history);
+        }
+    }
+}
--- src/KeyCdr.UI.Web/assets/js/react/dev/paths/history.jsx
+++ src/KeyCdr.UI.Web/assets/js/react/dev/paths/history.jsx
@@ -3,27 +3,54 @@ import ReactDOM from 'react-dom';
 import { Redirect} from 'react-router-dom'
 
 import ContentContainer from '../content.jsx';
+import Loading from '../loading.jsx';
 import SecureRouteComponent from '../secure_route_component.jsx';
 
 class History extends SecureRouteComponent {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isLoading: true,
+            data: this.getUserHistory()
+        }
     }
 
-    navTo(path) {
-        this.props.history.push(path);
+    getUserHistory() {
+        var token = this.base.auth.getCurrentToken();
+        return fetch('/api/User/History', {
+                method: "Post",
+                headers: { 'Content-Type': 'application/json', Accept: 'application/json' }
+                , body: JSON.stringify(token)
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                return Promise.resolve(data);
+            });
     }
 
     render() {
         if (this.authenticationComplete()) {
+
             return (
                 <ContentContainer authed={true}>
                     <h2>Session History</h2>
 
-                    <div className="spinner-border" role="status">
-                        <span className="sr-only">Loading...</span>
+                    <div className="content_row">
+                        <Loading showLoading={this.state.isLoading} />
+                        
+                        <table>
+                            <thead>
+                                <th>CreateDate</th>
+                            </thead>
+                            <tbody>
+                                {this.state.data != null &&
+                                    this.state.data.map((item, key) =>
+                                        <tr key={item.SessionId}>{item.CreateDate}</tr>
+                                )} 
+                            </tbody>
+                        </table>
                     </div>
-
                 </ContentContainer>
             );
         }
