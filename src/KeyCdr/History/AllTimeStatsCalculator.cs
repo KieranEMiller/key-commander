712d05220968b96bd6131fcf60aef46e6866a86d
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-16 21:11:17 +0000

    add a class to lookup and calculate the all time statistics for accuracy and speed

---
+++ src/KeyCdr/History/AllTimeStatsCalculator.cs
@@ -0,0 +1,85 @@
+﻿using KeyCdr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyCdr.History
+{
+    public class AllTimeStatsCalculator
+    {
+        public AllTimeStatsCalculator()
+        { }
+
+        public AllTimeStatsCalculator(Data.KCUser user)
+        {
+            _user = user;
+        }
+
+        private Data.KCUser _user;
+        private IList<Data.Session> _sessionsByUser;
+
+        public IList<Data.Session> GetUsersSessions()
+        {
+            if (_sessionsByUser == null)
+                _sessionsByUser = new UserSessionHistory().GetHistoryDetailsAllTime(_user);
+
+            return _sessionsByUser;
+        }
+
+        public SpeedModel GetSpeedAllTime()
+        {
+            IList<Data.Session> sessions = GetUsersSessions();
+
+            var existingSpeedAnalytics = sessions
+                .SelectMany(s => s.KeySequence
+                .SelectMany(ks => ks.KeySequenceAnalysis
+                    .Where(ksa => ksa.AnalysisTypeId.Equals((int)Analytics.AnalyticType.Speed))
+                .Select(speed => speed.AnalysisSpeed)));
+
+            //group by a constant lets you average all the properties
+            //in a single iteration...weird
+            var totals = existingSpeedAnalytics
+                .GroupBy(a => 1)
+                .Select(a => new SpeedModel(){
+                    CharsPerSec = a.Average(s => s.CharsPerSec),
+                    WordPerMin = a.Average(s => s.WordPerMin),
+                    TotalTimeInMilliSec = (int)a.Sum(s => s.TotalTimeInMilliSec),
+                    NumEntitiesRepresented = existingSpeedAnalytics.Count()
+                })
+                .Single();
+
+            return totals;
+        }
+
+        public AccuracyModel GetAccuracyAllTime()
+        {
+            IList<Data.Session> sessions = GetUsersSessions();
+
+            var existing = sessions
+                .SelectMany(s => s.KeySequence
+                .SelectMany(ks => ks.KeySequenceAnalysis
+                    .Where(ksa => ksa.AnalysisTypeId.Equals((int)Analytics.AnalyticType.Accuracy))
+                .Select(accuracy => accuracy.AnalysisAccuracy)));
+
+            //group by a constant lets you average all the properties
+            //in a single iteration...weird
+            var totals = existing
+                .GroupBy(a => 1)
+                .Select(a => new AccuracyModel() {
+                    Accuracy = a.Average(acc => acc.Accuracy),
+                    NumChars = a.Average(acc => acc.NumChars),
+                    NumCorrectChars = a.Average(acc => acc.NumCorrectChars),
+                    NumExtraChars = a.Average(acc => acc.NumExtraChars),
+                    NumIncorrectChars = a.Average(acc => acc.NumIncorrectChars),
+                    NumWords = a.Average(acc => acc.NumWords),
+                    NumShortChars = a.Average(acc => acc.NumShortChars),
+                    NumEntitiesRepresented = existing.Count()
+                })
+                .Single();
+
+            return totals;
+        }
+    }
+}
