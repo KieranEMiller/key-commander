eeadd1768d40c1d46dab46e29069015cb5548600
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-16 21:10:45 +0000

    add models for Speed and Accuracy Analytics.  they are more or less the same as the Data objects but without the circular reference problems.  currently just used as readonly containers of data on the web ui

---
+++ src/KeyCdr/Models/AccuracyModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeyCdr.Models
+{
+    public class AccuracyModel
+    {
+        public decimal Accuracy { get; set; }
+        public double NumWords { get; set; }
+        public double NumChars { get; set; }
+        public double NumCorrectChars { get; set; }
+        public double NumIncorrectChars { get; set; }
+        public double NumExtraChars { get; set; }
+        public double NumShortChars { get; set; }
+
+        //used for the all time model to reflect the total
+        //number of accuracies this was computed from
+        public int NumEntitiesRepresented { get; set; }
+
+        public static AccuracyModel Create(Data.AnalysisAccuracy orig)
+        {
+            return new AccuracyModel() {
+                Accuracy = orig.Accuracy,
+                NumChars = orig.NumChars,
+                NumCorrectChars = orig.NumCorrectChars,
+                NumExtraChars = orig.NumExtraChars,
+                NumIncorrectChars = orig.NumIncorrectChars,
+                NumShortChars = orig.NumShortChars,
+                NumWords = orig.NumWords,
+                NumEntitiesRepresented = 1
+            };
+        }
+    }
+}
---
+++ src/KeyCdr/Models/SpeedModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeyCdr.Models
+{
+    public class SpeedModel
+    {
+        public decimal TotalTimeInMilliSec { get; set; }
+        public decimal WordPerMin { get; set; }
+        public decimal CharsPerSec { get; set; }
+        
+        //used for the all time model to reflect the total
+        //number of accuracies this was computed from
+        public int NumEntitiesRepresented { get; set; }
+
+        public static SpeedModel Create(Data.AnalysisSpeed orig)
+        {
+            return new SpeedModel() {
+                CharsPerSec = orig.CharsPerSec,
+                TotalTimeInMilliSec = orig.CharsPerSec,
+                WordPerMin = orig.WordPerMin,
+                NumEntitiesRepresented = 1,
+            };
+        }
+    }
+}
