45e6af0b5da605b2c9e8fd1be33bba3f02804e88
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-18 20:14:33 +0000

    add new path/component for handling a new session/sequence

---
+++ src/KeyCdr.UI.Web/assets/js/react/dev/components/toolbar_new_sequence.jsx
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Stopwatch from './stopwatch.jsx';
+
+class ToolbarNewSequence extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var buttonText = (this.props.MaskIsVisible == true)
+            ? "Theatre Mode Off" : "Theatre Mode On";
+
+        var toolbarVisibilityCss = (this.props.IsRunning == true)
+            ? "toolbar_top_center" : "toolbar_top_center_hide";
+
+        return (
+            <div id="screen_mask_toolbar" className={toolbarVisibilityCss}>
+                <input onClick={this.props.onToggleMask} className="button-size-small" type="button" value={buttonText} />
+                <input onClick={this.props.onStopSequence} className="button-size-small" type="button" value="Stop Sequence" />
+                <Stopwatch IsRunning={this.props.IsRunning} />
+            </div>
+        );
+    }
+};
+export default ToolbarNewSequence;
---
+++ src/KeyCdr.UI.Web/assets/js/react/dev/paths/new_sequence.jsx
@@ -0,0 +1,130 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ContentContainer from '../content.jsx';
+import Loading from '../loading.jsx';
+import HideableScreenMask from '../components/hideable_screen_mask.jsx';
+import ToolbarNewSequence from '../components/toolbar_new_sequence.jsx';
+
+const ERROR_COUNTER_TEXT = [
+    "PRESS ENTER TWICE TO BEGIN (2 REMAINING)",
+    "PRESS ENTER TWICE TO BEGIN (1 REMAINING)...GET READY"
+];
+
+class NewSequence extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            EnterCounter: 0,
+            TextShown: ERROR_COUNTER_TEXT[0],
+            IsRunning: false,
+            IsLoading: false,
+            UseTheatreMode: false,
+            CurrentSequence: null
+        }
+    }
+
+    toggleTheatreMode = () => {
+        this.setState({ UseTheatreMode: !this.state.UseTheatreMode });
+    }
+
+    componentDidMount() {
+        this.inputSequenceField.focus();
+    }
+
+    inputKeyDown = (e) => {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            this.handleKeyEnter();
+        }
+    }
+
+    handleKeyEnter() {
+        if (this.state.IsRunning == true) {
+            this.stopSequence();
+        }
+        else if (this.state.EnterCounter == 1) {
+            this.startSequence();
+        }
+        else if (this.state.EnterCounter == 0) {
+            this.setState({
+                EnterCounter: 1,
+                TextShown: ERROR_COUNTER_TEXT[1]
+            });
+        }
+    }
+
+    startSequence() {
+        this.setState({ IsLoading: true });
+
+        fetch('/api/Sequence/GetNewSequence', {
+            method: "Get",
+            headers: { 'Content-Type': 'application/json', Accept: 'application/json' }
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log("new seq data", data);
+
+            this.setState({
+                CurrentSequence: data,
+                EnterCounter: 0,
+                TextShown: data.Text,
+                IsRunning: true,
+                IsLoading: false,
+                UseTheatreMode: true
+            });
+        });
+    }
+
+    stopSequence = () => {
+        this.setState({
+            IsRunning: false,
+            UseTheatreMode: false
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <HideableScreenMask MaskIsVisible={this.state.UseTheatreMode} />
+
+                <ToolbarNewSequence
+                    MaskIsVisible={this.state.UseTheatreMode}
+                    IsRunning={this.state.IsRunning}
+                    onToggleMask={this.toggleTheatreMode}
+                    onStopSequence={this.stopSequence}
+                />
+
+                <ContentContainer>
+                <h2>New Sequence</h2>
+                
+                <div className="content_row_sm show_above_mask">
+                    <Loading showLoading={this.state.IsLoading} />
+                    <h3>Text to Type</h3>
+                        
+                    <form>
+                        <textarea name="textShown"
+                            value={this.state.TextShown}
+                            readOnly
+                        />
+                    </form>
+                </div>
+
+                <div className="content_row_sm show_above_mask">
+                    <h3>Your Text</h3>
+
+                    <form>
+                        <textarea name="textEntered"
+                                ref={(input) => { this.inputSequenceField = input; }} 
+                                onKeyDown={this.inputKeyDown}
+                        />
+                    </form>
+                </div>
+                </ContentContainer>
+                </React.Fragment>
+        );
+    }
+}
+
+export default NewSequence;
