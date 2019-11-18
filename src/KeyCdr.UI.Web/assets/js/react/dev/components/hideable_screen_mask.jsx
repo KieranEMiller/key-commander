f0605574a0d2b5e925f4edace40e2a671cc4d580
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-18 20:15:48 +0000

    add new component for hiding and showing a mask that goes over the screen partially obscuring the background of the page.  currently used on the new session page when the user is typing in sample text

---
+++ src/KeyCdr.UI.Web/assets/js/react/dev/components/hideable_screen_mask.jsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HideableScreenMask extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var css = (this.props.MaskIsVisible) ? "loading_mask_screen" : "loading_mask_screen_hide";
+        return (
+            <div id="screen_mask" className={css}></div>
+        )
+    }
+}
+
+export default HideableScreenMask;
