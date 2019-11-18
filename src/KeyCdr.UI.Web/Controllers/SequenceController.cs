af528f435cb17b303e600341f7785f4c21aeeaa8
Kieran E. Miller <kieranemiller@gmail.com>
2019-11-18 20:21:11 +0000

    add new controller and model for starting and looking up text for a new sequence

---
+++ src/KeyCdr.UI.Web/Controllers/SequenceController.cs
@@ -0,0 +1,29 @@
+﻿using KeyCdr.TextSamples;
+using KeyCdr.UI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KeyCdr.UI.Web.Controllers
+{
+    public class SequenceController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetNewSequence()
+        {
+            WikipediaTextGenerator gen = new WikipediaTextGenerator();
+            var sample = gen.GetParagraph();
+
+            var result = new NewSequenceModel() {
+                Text = sample.GetText(),
+                SourceKey = sample.GetSourceKey(),
+                SourceType = sample.GetSourceType().ToString()
+            };
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+    }
+}
---
+++ src/KeyCdr.UI.Web/Models/NewSequenceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeyCdr.UI.Web.Models
+{
+    public class NewSequenceModel
+    {
+        public Guid SessionId { get; set; }
+
+        public string Text { get; set; }
+        public string SourceKey { get; set; }
+        public string SourceType { get; set; }
+    }
+}
